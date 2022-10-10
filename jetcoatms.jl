d325c54bfed53f036d787c13689bf868a17301d9
Automated <actions@users.noreply.github.com>
2022-10-10 21:41:14 +0000

    Latest data: Mon Oct 10 21:41:14 UTC 2022

--- jetcoatms.jl
+++ jetcoatms.jl
@@ -47064,28 +47064,6 @@
   ]
 }
 {
-  "id": 288400,
-  "latitude": 22.185607,
-  "longitude": 113.547978,
-  "bank": "Bank of China Limited, Macau Branch",
-  "address": "MGM Cotai, Avenida da Nave Desportiva, Taipa",
-  "tran": [
-    "Cash Withdrawal, Fund Transfer, Balance Enquiry, PIN Change and other basic services",
-    "Bill Payment, Credit Card Payment and Charity Donation",
-    "Diners Club",
-    "Interbank Transfer",
-    "Discover",
-    "FISC",
-    "CIRRUS",
-    "CUP",
-    "JCB",
-    "PLUS"
-  ],
-  "currencies": [
-    "HKD"
-  ]
-}
-{
   "id": 288401,
   "latitude": 22.185607,
   "longitude": 113.547978,
